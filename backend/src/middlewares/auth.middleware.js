fcf00de21a3b88367117f733e666f230cfd03fd6
Antonio Manuel Mérida Borrero <anmamebo2001@gmail.com>
2024-11-20 01:08:44 +0100

    Backend - Fix auth middleware

--- backend/src/middlewares/auth.middleware.js
+++ backend/src/middlewares/auth.middleware.js
@@ -1,16 +1,26 @@
 const jwt = require("jsonwebtoken");
 
 const verifyToken = (req, res, next) => {
-  const token = req.headers["authorization"];
+  const authHeader = req.headers["authorization"];
 
-  if (!token) return res.status(403).send({ message: "No token provided" });
+  if (!authHeader)
+    return res.status(403).send({ message: "No token provided" });
+
+  const token = authHeader.split(" ")[1];
+
+  if (!token) {
+    return res.status(403).send({ message: "Invalid token format" });
+  }
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
     req.userId = decoded.id;
     next();
   } catch (error) {
-    res.status(401).send({ message: "Invalid or expired token" });
+    res
+      .status(401)
+      .send({ message: "Invalid or expired token:" + error.message });
   }
 };
 
