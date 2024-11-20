48a5b2952f7ef623661fa9d00a33245b6d478724
Antonio Manuel Mérida Borrero <anmamebo2001@gmail.com>
2024-11-20 19:11:19 +0100

    Frontend - Create AuthContext

---
+++ frontend/src/context/AuthContext.jsx
@@ -0,0 +1,28 @@
+import { createContext, useState } from "react";
+
+const AuthContext = createContext();
+
+export default AuthContext;
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [token, setToken] = useState(localStorage.getItem("token") || null);
+
+  const login = (token, userData) => {
+    localStorage.setItem("token", token);
+    setToken(token);
+    setUser(userData);
+  };
+
+  const logout = () => {
+    localStorage.removeItem("token");
+    setToken(null);
+    setUser(null);
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, token, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
