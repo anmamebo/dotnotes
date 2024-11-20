7f16e8f70c29eddeaf2842d56b032c659dfea6d5
Antonio Manuel Mérida Borrero <anmamebo2001@gmail.com>
2024-11-21 00:38:50 +0100

    Frontend - Implement create new note

--- frontend/src/pages/Notes.jsx
+++ frontend/src/pages/Notes.jsx
@@ -1,10 +1,29 @@
 import { useContext, useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
 import axios from "../config/axios";
 import AuthContext from "../context/AuthContext";
 
 const Notes = () => {
   const { user, logout } = useContext(AuthContext);
   const [notes, setNotes] = useState([]);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm();
+  const [message, setMessage] = useState("");
+
+  const onSubmit = async (data) => {
+    try {
+      const res = await axios.post("/notes", data);
+      setNotes([...notes, res.data.note]);
+      setMessage(res.data.message);
+      reset();
+    } catch (err) {
+      setMessage(err.response?.data?.message);
+    }
+  };
 
   useEffect(() => {
     const fetchNotes = async () => {
@@ -22,8 +41,42 @@ const Notes = () => {
   return (
     <div>
       <h1>{`Welcome, ${user?.name || "User"}!`}</h1>
-      <h2>Your Notes</h2>
 
+      <h2>Create Note</h2>
+      <form
+        className="flex flex-col items-center w-full gap-4"
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <div className="flex flex-col w-full">
+          <label className="text-md">Title</label>
+          <input
+            type="text"
+            className="mt-1 border-b p-2 py-1"
+            id="title"
+            {...register("title", { required: true })}
+          />
+          {errors.title && <span>This field is required</span>}
+        </div>
+
+        <div className="flex flex-col w-full">
+          <label className="text-md">Content</label>
+          <input
+            type="text"
+            className="mt-1 border-b p-2 py-1"
+            id="content"
+            {...register("content", { required: true })}
+          />
+          {errors.content && <span>This field is required</span>}
+        </div>
+
+        <button type="submit" className="px-4 py-2 border-black border w-1/2">
+          Crear
+        </button>
+
+        {message && <span>{message}</span>}
+      </form>
+
+      <h2>Your Notes</h2>
       {/* Note list */}
       <ul>
         {notes.map((note) => (
