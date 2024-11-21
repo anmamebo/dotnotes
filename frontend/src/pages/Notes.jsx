bd883856d09e2f7e5f3654c9a258b1b2a1497286
Antonio Manuel Mérida Borrero <anmamebo2001@gmail.com>
2024-11-21 01:25:36 +0100

    Frontend - Create and use header component

--- frontend/src/pages/Notes.jsx
+++ frontend/src/pages/Notes.jsx
@@ -1,10 +1,9 @@
-import { useContext, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
+import Header from "../components/Header";
 import axios from "../config/axios";
-import AuthContext from "../context/AuthContext";
 
 const Notes = () => {
-  const { user, logout } = useContext(AuthContext);
   const [notes, setNotes] = useState([]);
   const {
     register,
@@ -39,54 +38,61 @@ const Notes = () => {
   }, []);
 
   return (
-    <div>
-      <h1>{`Welcome, ${user?.name || "User"}!`}</h1>
+    <>
+      <Header />
 
-      <h2>Create Note</h2>
-      <form
-        className="flex flex-col items-center w-full gap-4"
-        onSubmit={handleSubmit(onSubmit)}
-      >
-        <div className="flex flex-col w-full">
-          <label className="text-md">Title</label>
-          <input
-            type="text"
-            className="mt-1 border-b p-2 py-1"
-            id="title"
-            {...register("title", { required: true })}
-          />
-          {errors.title && <span>This field is required</span>}
+      <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4">
+        <div className="flex flex-col">
+          <h2>Create Note</h2>
+          <form
+            className="flex flex-col items-center w-full gap-4"
+            onSubmit={handleSubmit(onSubmit)}
+          >
+            <div className="flex flex-col w-full">
+              <label className="text-md">Title</label>
+              <input
+                type="text"
+                className="mt-1 border-b p-2 py-1"
+                id="title"
+                {...register("title", { required: true })}
+              />
+              {errors.title && <span>This field is required</span>}
+            </div>
+
+            <div className="flex flex-col w-full">
+              <label className="text-md">Content</label>
+              <input
+                type="text"
+                className="mt-1 border-b p-2 py-1"
+                id="content"
+                {...register("content", { required: true })}
+              />
+              {errors.content && <span>This field is required</span>}
+            </div>
+
+            <button
+              type="submit"
+              className="px-4 py-2 border-black border w-1/2"
+            >
+              Crear
+            </button>
+
+            {message && <span>{message}</span>}
+          </form>
+
+          <h2>Your Notes</h2>
+          {/* Note list */}
+          <ul>
+            {notes.map((note) => (
+              <li key={note._id}>
+                <h3>{note.title}</h3>
+                <p>{note.content}</p>
+              </li>
+            ))}
+          </ul>
         </div>
-
-        <div className="flex flex-col w-full">
-          <label className="text-md">Content</label>
-          <input
-            type="text"
-            className="mt-1 border-b p-2 py-1"
-            id="content"
-            {...register("content", { required: true })}
-          />
-          {errors.content && <span>This field is required</span>}
-        </div>
-
-        <button type="submit" className="px-4 py-2 border-black border w-1/2">
-          Crear
-        </button>
-
-        {message && <span>{message}</span>}
-      </form>
-
-      <h2>Your Notes</h2>
-      {/* Note list */}
-      <ul>
-        {notes.map((note) => (
-          <li key={note._id}>
-            <h3>{note.title}</h3>
-            <p>{note.content}</p>
-          </li>
-        ))}
-      </ul>
-    </div>
+      </main>
+    </>
   );
 };
 
