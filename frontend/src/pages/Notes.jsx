a67434b1b0e3e32e1b1a23099507e04c3e3a839f
Antonio Manuel Mérida Borrero <anmamebo2001@gmail.com>
2024-11-27 00:58:09 +0100

    Frontend - Display messages when edit or delete notes

--- frontend/src/pages/Notes.jsx
+++ frontend/src/pages/Notes.jsx
@@ -6,6 +6,7 @@ import axios from "../config/axios";
 
 const Notes = () => {
   const [notes, setNotes] = useState([]);
+  const [message, setMessage] = useState("");
 
   useEffect(() => {
     const fetchNotes = async () => {
@@ -13,7 +14,7 @@ const Notes = () => {
         const res = await axios.get("/notes");
         setNotes(res.data);
       } catch (err) {
-        console.log(err);
+        setMessage(err.response.data.message);
       }
     };
 
@@ -28,9 +29,17 @@ const Notes = () => {
           note._id === id ? { ...note, ...updatedNote } : note
         )
       );
-      console.log(res.data.message);
+      setMessage(res.data.message);
+
+      setTimeout(() => {
+        setMessage("");
+      }, 3000);
     } catch (err) {
-      console.log(err);
+      setMessage(err.response.data.message);
+
+      setTimeout(() => {
+        setMessage("");
+      }, 3000);
     }
   };
 
@@ -38,9 +47,18 @@ const Notes = () => {
     try {
       const res = await axios.delete(`/notes/${id}`);
       setNotes(notes.filter((note) => note._id !== id));
-      console.log(res.data.message);
+
+      setMessage(res.data.message);
+
+      setTimeout(() => {
+        setMessage("");
+      }, 3000);
     } catch (err) {
-      console.log(err);
+      setMessage(err.response.data.message);
+
+      setTimeout(() => {
+        setMessage("");
+      }, 3000);
     }
   };
 
@@ -51,7 +69,16 @@ const Notes = () => {
       <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4">
         <div className="flex flex-col gap-3">
           {/* Title */}
-          <h1 className="font-semibold text-xl">Your notes</h1>
+          <div className="flex items-center justify-between">
+            <h1 className="font-semibold text-2xl">Your notes</h1>
+
+            {/* Messages */}
+            {message && (
+              <span className="bg-black text-white rounded px-10 py-1">
+                {message}
+              </span>
+            )}
+          </div>
 
           {/* Notes container */}
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
